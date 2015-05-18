cd4ce1b59e03361f2f04086df814c0e526ef56ff
Chris Maguire <cwmaguire@gmail.com>
2015-05-18 12:41:21 -0700

    Initial commit
    Created a bezier animation script and an FP library to treat
    arrays as lists

---
+++ bezier.js
@@ -0,0 +1,153 @@
+function demo(){
+  l1 = {p1: {x: 10, y: 10}, p2: {x: 15, y: 100}};
+  l2 = {p1: {x: 100, y: 20}, p2: {x: 80, y: 90}};
+  bezier(l1, l2);
+};
+
+
+function bezier(line1, line2){
+  var segments = 100;
+  line1Segs = lineSegments(line1, segments);
+  line2Segs = lineSegments(line2, segments);
+  bezLines = bezierLines(line1Segs, line2Segs);
+  bezSegs = bezSegments(bezLines, segments);
+  //steps = zip3(line1Segs, line2Segs, bezSegs);
+  //foldl(animateLine, bezSegs, []);
+  animateLine(bezSegs);
+}
+
+function animateLine(lineSegs){
+  var canvas = document.getElementById("bezier_canvas");
+
+  if (canvas.getContext) {
+    ctx = canvas.getContext("2d");
+    blankCanvas(ctx, canvas);
+    foldl(function(curr, prevs){
+            var lines = cons(prevs, curr);
+            map(function(line){
+                  drawLine(ctx, line)
+                },
+                lines);
+            return lines;
+          },
+          steps,
+          []);
+  }
+}
+
+function drawLine(ctx, line){
+  ctx.moveTo(line.x1, line.y1);
+  ctx.lineTo(line.x2, line.y2);
+  ctx.stroke();
+}
+
+function blankCanvas(ctx, canvas){
+    drawRect(ctx,
+             {x: 0, y: 0,
+              h: canvas.height, w: canvas.width,
+              r: 255, g: 255, b: 255, a: 1.0,
+              fill: "color"});
+}
+
+function line(x1, y1, x2, y2){
+  return {p1: {x: x1, y: y1}, p2: {x: x2, y: y2}};
+}
+
+function lineSegments(l, numSegments){
+  //var xDist = Math.abs(l.p1.x - l.p2.x) / numSegments;
+  //var yDist = Math.abs(l.p1.y - l.p2.y) / numSegments;
+  //var xFun = plusOrMinus(p1.x, p2.x);
+  //var yFun = plusOrMinus(p1.y, p2.y);
+  var xDist = (l.p1.x - l.p2.x) / numSegments;
+  var yDist = (l.p1.y - l.p2.y) / numSegments;
+  var segmentNumbers = seq(numSegments);
+  //var fun = function(seg){
+                //return line(xFun(l.p1.x, ((seg - 1) * xDist)),
+                            //yFun(l.p1.y, ((seg - 1) * yDist)),
+                            //xFun(l.p1.x, (seg * xDist)),
+                            //yFun(l.p1.y, (seg * yDist)));
+            //};
+  /*
+   * x1 - x2 will be positive or negative depending on what direction
+   * the line is going. If it's negative then dividing by the number
+   * of segments will give a negative segment length.
+   * Multiplying a negative segment length by the segment number
+   * will give us the amounts to go in the proper (up or left) direction.
+   */
+  var fun = function(seg){
+                return line(l.p1.x + ((seg - 1) * xDist),
+                            l.p1.y + ((seg - 1) * yDist),
+                            l.p1.x + (seg * xDist),
+                            l.p1.y + (seg * yDist));
+            };
+  return map(fun, segmentNumbers);
+}
+
+function plusOrMinus(a, b){
+  if(a < b){
+    return function(f, g){ return f + g };
+  }else{
+    return function(f, g){ return f - g };
+  }
+}
+
+function testLineSegments(){
+  var l = line(10, 10, 20, 20);
+  var segs = lineSegments(l, 2);
+  return segs[0].p1.x == 10 &&
+         segs[0].p1.y == 10 &&
+         segs[0].p2.x == 15 &&
+         segs[0].p2.y == 15 &&
+         segs[1].p1.x == 15 &&
+         segs[1].p1.y == 15 &&
+         segs[1].p2.x == 20 &&
+         segs[1].p2.y == 20;
+}
+
+function bezierLines(segs1, segs2){
+  var points1 = reverse(cons(map(end, segs1), start(hd(segs1))));
+  var lastSeg2End = end(hd(reverse(segs2)));
+  var points2 = concat(map(start, segs2), [lastSeg2End]);
+  var lines = map(function(pair){
+                    return {p1: pair[0], p2: pair[1]}
+                  },
+                  zip(points1, points2));
+  return lines;
+}
+
+function bezSegments(lines, numSegments){
+  var segSeq = seq(numSegments);
+  var lineCurrTotals = zip3(lines, segSeq, repeat(numSegments));
+  return map(lineSegmentN, lineCurrTotals);
+}
+
+function bezSegmentsTest(){
+  var line1 = line(10, 10, 20, 20);
+  var line2 = line(20, 20, 30, 10);
+  var bezSegs = bezSegments([line1, line2], 2);
+  var seg1 = bezSegs[0];
+  var seg2 = bezSegs[1];
+  return seg1.p1.x == 10 &&
+         seg1.p1.y == 10 &&
+         seg1.p2.x == 15 &&
+         seg1.p2.y == 15 &&
+         seg2.p1.x == 25 &&
+         seg2.p1.y == 15 &&
+         seg2.p2.x == 30 &&
+         seg2.p2.y == 10;
+}
+
+function lineSegmentN(lineCurrTotal){
+  var line = lineCurrTotal[0];
+  var segNo = lineCurrTotal[1];
+  var segTotal = lineCurrTotal[2];
+  return lineSegments(line, segTotal)[segNo];
+}
+
+function start(line){
+  return line.p1;
+}
+
+function end(line){
+  return line.p2;
+}
---
+++ lists.js
@@ -0,0 +1,176 @@
+function hd(arr){
+  return arr[0];
+}
+
+function tl(arr){
+  return arr.slice(1);
+}
+
+function tlTest(){
+  var arr = [1,2,3];
+  var tail = tl(arr);
+  return arr[0] == 1 &&
+         arr[1] == 2 &&
+         arr[2] == 3 &&
+         arr.length == 3 &&
+         tail[0] == 2 &&
+         tail[1] == 3 &&
+         tail.length == 2;
+}
+
+function cons(arr, elem){
+  copy = arr.slice(0);
+  copy.unshift(elem);
+  return copy;
+}
+
+function consTest(){
+  var arr = [1,2,3];
+  var arr2 = cons(arr, 0);
+  return arr.length == 3 &&
+         arr[0] == 1 &&
+         arr[1] == 2 &&
+         arr[2] == 3 &&
+         arr2.length == 4 &&
+         arr2[0] == 0 &&
+         arr2[1] == 1 &&
+         arr2[2] == 2 &&
+         arr2[3] == 3;
+}
+
+function reverse(arr){
+  copy = arr.slice(0);
+  copy.reverse();
+  return copy;
+}
+
+function reverseTest(){
+  var arr = [1,2,3];
+  var rev = reverse(arr);
+  return arr[0] == 1 &&
+         arr[1] == 2 &&
+         arr[2] == 3 &&
+         rev[0] == 3 &&
+         rev[1] == 2 &&
+         rev[2] == 1;
+}
+
+function concat(arr1, arr2){
+  return arr1.concat(arr2);
+}
+
+function map(fun, arr, maybe_results){
+  var results = (maybe_results == undefined ? [] : maybe_results.slice(0));
+  var next;
+  if(arr.length == 0){
+    return reverse(results);
+  }else{
+    return map(fun, tl(arr), cons(results, fun(hd(arr))));
+  }
+}
+
+function mapTest(){
+  var result = map(function(x){ return x + 1 }, seq(3));
+  return result[0] == 2 &&
+         result[1] == 3 &&
+         result[2] == 4;
+}
+
+function seq(maybe_next, maybe_end, maybe_sequence){
+  var next;
+  var end;
+  var sequence;
+  if(maybe_end == undefined){
+    end = maybe_next;
+    next = 1;
+  }else{
+    next = maybe_next;
+    end = maybe_end;
+  }
+  if(maybe_sequence == undefined){
+    sequence = []
+  }else{
+    sequence = maybe_sequence;
+  }
+  if(next > end){
+    return sequence;
+  }else{
+    sequence.push(next);
+    return seq(next + 1, end, sequence);
+  }
+}
+
+function repeat(elem, count){
+  var sequence = seq(count);
+  return map(function(x){ return elem }, sequence);
+}
+
+function repeatTest(){
+  var arr = repeat('x', 4);
+  return arr[0] == 'x' &&
+         arr[1] == 'x' &&
+         arr[2] == 'x' &&
+         arr[3] == 'x';
+}
+
+function foldl(fun, arr, acc){
+  if(arr.length == 0){
+    return acc;
+  }
+  acc_copy = acc.slice(0);
+  return foldl(fun, tl(arr), cons(acc, fun(hd(arr), acc_copy)));
+}
+
+function zip(arr1, arr2){
+  return zip_(arr1, arr2, []);
+}
+
+function zip_(arr1, arr2, zips){
+  if(arr1.length == 0 ||
+     arr2.length == 0){
+    return reverse(zips);
+  }
+
+  return zip_(tl(arr1),
+              tl(arr2),
+              cons(zips, [hd(arr1), hd(arr2)]));
+
+}
+
+function zip3(arr1, arr2, arr3){
+  return zip3_(arr1, arr2, arr3, []);
+}
+
+function zip3_(arr1, arr2, arr3, zips){
+  if(arr1.length == 0 ||
+     arr2.length == 0 ||
+     arr3.length == 0){
+    return reverse(zips);
+  }
+
+  return zip3_(tl(arr1),
+              tl(arr2),
+              tl(arr3),
+              cons(zips, [hd(arr1), hd(arr2), hd(arr3)]));
+}
+
+function zip3Test(){
+  var arr1 = [1,2,3];
+  var arr2 = [4,5,6];
+  var arr3 = [7,8,9];
+
+  var zipped = zip3(arr1, arr2, arr3);
+  return zipped[0][0] == 1 &&
+         zipped[0][1] == 4 &&
+         zipped[0][2] == 7 &&
+         zipped[1][0] == 2 &&
+         zipped[1][1] == 5 &&
+         zipped[1][2] == 8 &&
+         zipped[2][0] == 3 &&
+         zipped[2][1] == 6 &&
+         zipped[2][2] == 9 &&
+         zipped.length == 3 &&
+         zipped[0].length == 3 &&
+         zipped[1].length == 3 &&
+         zipped[2].length == 3;
+}
